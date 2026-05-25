4a008684bb7e47203511cbcac09657093c6373c6
aitm <241629916+aitmcs@users.noreply.github.com>
2026-05-25 23:30:18 +0530

    Create LAB2.asm

---
+++ LAB2.asm
@@ -0,0 +1,12 @@
+AREA OPERATIONS, CODE, READONLY
+        ENTRY
+START
+        LDR  R0, =5
+        LDR  R1, =7
+        ADD  R2, R0, R1
+        MUL  R3, R0, R1
+        ORR  R4, R0, R1
+        EOR  R5, R0, R1
+        NOP
+        NOP
+        END
